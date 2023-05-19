01f1668e988570c93663a20dabb995d706561133
Nasuta <Nasuta@users.noreply.github.com>
2023-05-19 18:40:04 +0200

    Add files via upload

--- jQuery/main45764.js
+++ jQuery/main45764.js
@@ -16,7 +16,7 @@
 $(document).ready(function() {
   $('#button').click(function() {
       $('#square').animate({
-          left: '+=100px',
+          left: '-100px',
           width: '100px',
           height: '100px'
       }, 3000, function() {
